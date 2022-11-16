046a7a58b69364996db5341efd32d4445bade8da
Facundo Garcia <facundogarcia-ae@team.asana.com>
2022-11-15 23:26:21 -0300

    add page nav interactions

--- src/server/Utils.ts
+++ src/server/Utils.ts
@@ -5,7 +5,10 @@ export function getNextAndLast({chapters, chapter, page}: PageFunctionProps) {
 	const isLast = chapter === chapters.length && page === lastChapter.pages.length
 	if (isLast) return null
 	return {
-		one: page === currentChapter({chapters, chapter}).pages.length ? {chapter: chapter + 1, page: 0} : {chapter, page: page + 1},
+		one: page === currentChapter({chapters, chapter}).pages.length ? {chapter: chapter + 1, page: 0} : {
+			chapter,
+			page: page + 1
+		},
 		all: {chapter: chapters.length, page: lastChapter.pages.length}
 	}
 }
@@ -14,7 +17,10 @@ export function getPreviousAndFirst({chapters, chapter, page}: PageFunctionProps
 	const isFirst = chapter === 1 && page === 0
 	if (isFirst) return null
 	return {
-		one: page === 0 ? {chapter: chapter - 1, page: previousChapter({chapters, chapter}).pages.length} : {chapter, page: page - 1},
+		one: page === 0 ? {chapter: chapter - 1, page: previousChapter({chapters, chapter}).pages.length} : {
+			chapter,
+			page: page - 1
+		},
 		all: {chapter: 1, page: 0}
 	}
 }
@@ -29,13 +35,19 @@ function last<T>(list: ReadonlyArray<T>): T {
 	return list[list.length - 1]
 }
 
-function previousChapter({chapters, chapter}: {chapters: ReadonlyArray<Chapter>, chapter: number}) {
+function previousChapter({chapters, chapter}: { chapters: ReadonlyArray<Chapter>, chapter: number }) {
 	return chapters[chapter - 2]
 }
 
-function currentChapter({chapters, chapter}: {chapters: ReadonlyArray<Chapter>, chapter: number}) {
+function currentChapter({chapters, chapter}: { chapters: ReadonlyArray<Chapter>, chapter: number }) {
 	return chapters[chapter - 1]
 }
 
 type PageFunctionProps = { chapters: ReadonlyArray<Chapter>, chapter: number, page: number }
 export type PageCoordinates = { chapter: number, page: number }
+export type NavigablePages = { one: PageCoordinates, all: PageCoordinates }
+export type RelatedPages = {
+	backward: NavigablePages | null
+	current: PageCoordinates
+	forward: NavigablePages | null
+}
--- src/server/actions/GetAllPages.ts
+++ src/server/actions/GetAllPages.ts
@@ -2,28 +2,26 @@ import {getChapters} from "@server/GetChapters"
 import {getImage, getNextAndLast, getPreviousAndFirst} from "@server/Utils"
 
 export default async function getAllPages() {
- const locale = "en-US"
+	const locale = "en-US"
 
 	const chapters = await getChapters({locale})
 
-    //const locales = ["en-US", "es"]
-    const chapterNumbers = chapters.flatMap((chapter, i) => [
-        {chapter: i + 1, page: 0},
-        ...chapter.pages.map((_, j) => ({chapter: i + 1, page: j + 1}))
-    ])
-    const paths = chapterNumbers.map(params => {
-	    const {chapter, page} = params
-	    const image = getImage({chapters, chapter, page})
-        const props = {
-                image,
-                backward: getPreviousAndFirst({chapters, chapter, page}),
-                current: {chapter: chapter, page: page},
-                forward: getNextAndLast({chapters, chapter, page}),
-                chapter: chapter,
-                page: page
-            }
-        return  {params: {chapter: `${chapter}`, page: `${page}`}, props}
-    })
-    //const localized = paths.flatMap(path => locales.map(locale => ({...path, locale})))
-    return paths
+	//const locales = ["en-US", "es"]
+	const chapterNumbers = chapters.flatMap((chapter, i) => [
+		{chapter: i + 1, page: 0},
+		...chapter.pages.map((_, j) => ({chapter: i + 1, page: j + 1}))
+	])
+	const paths = chapterNumbers.map(params => {
+		const {chapter, page} = params
+		const image = getImage({chapters, chapter, page})
+		const props = {
+			image,
+			backward: getPreviousAndFirst({chapters, chapter, page}),
+			current: {chapter: chapter, page: page},
+			forward: getNextAndLast({chapters, chapter, page})
+		}
+		return {params: {chapter: `${chapter}`, page: `${page}`}, props}
+	})
+	//const localized = paths.flatMap(path => locales.map(locale => ({...path, locale})))
+	return paths
 }
