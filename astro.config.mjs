4856dbcf69c2ace8ea23681e2b5cb119d6c8ae74
Facundo Garcia <facundogarcia-ae@team.asana.com>
2022-11-16 13:29:12 -0300

    apply image optimizing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
