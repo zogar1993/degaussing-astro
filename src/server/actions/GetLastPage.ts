39f8b7e446604f9a1a873558ed10a1bacf415c8e
Facundo Garcia <facundogarcia-ae@team.asana.com>
2022-11-18 17:28:08 -0300

    add localization remove react

--- src/server/actions/GetLastPage.ts
+++ src/server/actions/GetLastPage.ts
@@ -1,9 +1,8 @@
 import {getChapters} from "@server/GetChapters"
 import getPageInfo from "@server/GetPageInfo"
 
-export default async function getLastPage() {
-	const locale = "en-US"
-	const chapters = await getChapters({locale})
+export default async function getLastPage({language}: {language: string}) {
+	const chapters = await getChapters({language})
 
 	const chapter = chapters.length
 	const page = chapters[chapters.length - 1].pages.length
