0ea1662123bc5e0bd4ec5111eda3f43330b83705
Facundo Garcia <facundogarcia-ae@team.asana.com>
2022-11-19 01:44:04 -0300

    test removing internationalization

--- src/server/actions/GetCharactersInfo.ts
+++ src/server/actions/GetCharactersInfo.ts
@@ -2,5 +2,5 @@ import {getEntries} from "@server/GetEntries"
 import type {Character} from "@transport/Character"
 
 export default async function getCharactersInfo({language}: {language: string}) {
-	return  getEntries<Character>("biography", language)
+	return getEntries<Character>("biography", language)
 }
