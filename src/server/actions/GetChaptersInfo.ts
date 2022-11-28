c9e02138c648e0c441c51c6abdc9b0d4d6b97844
Facundo Garcia <facundogarcia-ae@team.asana.com>
2022-11-28 15:54:39 -0300

    style chapters page

--- src/server/actions/GetChaptersInfo.ts
+++ src/server/actions/GetChaptersInfo.ts
@@ -3,9 +3,10 @@ import {getChapters} from "@server/GetChapters"
 export default async function getChaptersInfo({language}: {language: string}) {
 	const chapters = await getChapters({language})
 
-	return chapters.map((chapter) => ({
+	return chapters.map((chapter, i) => ({
 		name: chapter.name,
 		cover: chapter.cover,
+		number: i + 1,
 		pagesAmount: chapter.pages.length
 	}))
 }
