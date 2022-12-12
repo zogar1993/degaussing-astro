df0ef046e7dc7b2492ca5e9d8bd777683eaf42b8
Facundo Garcia <zogar1993@gmail.com>
2022-12-12 20:01:17 -0300

    update astro i18n version

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -2,4 +2,9 @@
 export default {
   defaultLocale: "en-US",
   locales: ["en-US", "es"],
+  i18nextServer: {
+    backend: {
+      loadPath: "./public/locales/{{lng}}/{{ns}}.json",
+    },
+  },
 };
