1adf382e91c22aa3ff9317f1e0c04d038861bbea
Facundo Garcia <zogar1993@gmail.com>
2022-12-15 12:02:04 -0300

    move characters to ordered contentful list

--- src/server/actions/GetContributionLinks.ts
+++ src/server/actions/GetContributionLinks.ts
@@ -2,5 +2,5 @@ import {getEntries} from "@server/GetEntries"
 import type {Link} from "@transport/Link"
 
 export async function getContributionLinks(): Promise<ReadonlyArray<Link>> {
-		return (await getEntries<any>("links")).find(links => links.code === "contribution").links
+		return (await getEntries<any>("list")).find(links => links.name === "contribution").items
 }
--- src/server/actions/GetSocialLinks.ts
+++ src/server/actions/GetSocialLinks.ts
@@ -2,5 +2,5 @@ import {getEntries} from "@server/GetEntries"
 import type {Link} from "@transport/Link"
 
 export async function getSocialLinks(): Promise<ReadonlyArray<Link>> {
-		return (await getEntries<any>("links")).find(links => links.code === "social").links
+		return (await getEntries<any>("list")).find(links => links.name === "social").items
 }
