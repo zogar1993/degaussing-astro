cc9593aa9170a357e7dfe9b78ffb5f31566bc662
Facundo Garcia <facundogarcia-ae@team.asana.com>
2023-02-11 14:57:59 -0300

    :bulb: Fix local so that latin characters show correctly

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -2,6 +2,7 @@
 export default {
   defaultLocale: "en",
   locales: ["en", "es"],
+  //Check on Windows to see if it still breaks: https://github.com/yassinedoghri/astro-i18next/issues/79#issuecomment-1348369866
   i18nextServer: {
     backend: {
       loadPath: "./public/locales/{{lng}}/{{ns}}.json",
