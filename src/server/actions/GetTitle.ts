4ab020c935af0dc465f5e9b1893b510e40306e28
Facundo Garcia <facundogarcia-ae@team.asana.com>
2023-03-06 22:21:18 -0300

    :wheelchair: Add h1 with same text as title

--- src/server/actions/GetTitle.ts
+++ src/server/actions/GetTitle.ts
@@ -1,19 +1,7 @@
+import getTitleParts from "@server/actions/GetTitleParts"
 import type {RelatedPages} from "@server/Utils"
-import {t} from "i18next"
-import getMenuItems from "@server/actions/GetMenuItems"
 
 export default function getTitle({path, info}: { path: string, info?: RelatedPages }) {
-	const items = getMenuItems({path})
-	const item = items.find(item => item.selected)
-
-	if (item) return [item.text]
-	if (info) {
-		const page = info.current.page
-		const chapter = info.current.chapter
-		return [
-			`${t("strip.chapter")} ${chapter}`,
-			`${page === 0 ? t("strip.cover") : `${t("strip.page")} ${page}`}`
-		]
-	}
-	throw Error(`No title for page ${path}`)
+	const parts = ["Degaussing Comic", ...getTitleParts({path, info})]
+	return parts.reverse().join(" | ")
 }
