ea388827da4002261dec0166dd2a23ae651d127c
Facundo Garcia <facundogarcia-ae@team.asana.com>
2023-03-19 21:27:10 -0300

    :coffin: remove old fix for astro i18next

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
   defaultLocale: "en",
-  locales: ["en", "es"],
-  //Check on Windows to see if it still breaks: https://github.com/yassinedoghri/astro-i18next/issues/79#issuecomment-1348369866
-  i18nextServer: {
-    backend: {
-      loadPath: "./public/locales/{{lng}}/{{ns}}.json",
-    },
-  },
+  locales: ["en", "es"]
 };
