f13453c3af593ada47f2ae234dea33c45c8db2e6
Facundo Garcia <facundogarcia-ae@team.asana.com>
2023-12-07 18:38:46 -0300

    :arrow_up: up astro dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config"
 import astroI18next from "astro-i18next"
-import image from "@astrojs/image"
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
-		image({ serviceEntryPoint: "@astrojs/image/sharp" }),
 		astroI18next()
 	]
 })
