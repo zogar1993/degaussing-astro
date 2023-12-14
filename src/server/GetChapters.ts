031da675c79ba0e33ad49ae9eb9a73feb0a65587
Facundo Garcia <facundogarcia-ae@team.asana.com>
2023-12-14 03:28:21 -0300

    :lipstick: add background image so that it shows while background is loading

--- src/server/GetChapters.ts
+++ src/server/GetChapters.ts
@@ -1,11 +1,7 @@
 import type {Chapter} from "@transport/Chapter"
 import {getList} from "@server/GetEntries"
 
-export async function getChapters({
-																		language
-																	}: {
-	language: string
-}): Promise<ReadonlyArray<Chapter>> {
+export async function getChapters({language}: { language: string }): Promise<ReadonlyArray<Chapter>> {
 	const chapters = await getList<Chapter>("chapters", language)
 	return chapters.map((chapter, i) => ({...chapter, number: i + 1}))
 }
