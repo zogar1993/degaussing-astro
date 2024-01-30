d12d22188e06a217b257dc615f715c8b83a1426a
Facundo Garcia <facundogarcia-ae@team.asana.com>
2024-01-30 01:06:16 -0300

    :zap: optimize characters image size

--- src/server/GetChapters.ts
+++ src/server/GetChapters.ts
@@ -1,7 +1,14 @@
+import {imageToUrl} from "@server/Utils"
 import type {Chapter} from "@transport/Chapter"
 import {getList} from "@server/GetEntries"
 
 export async function getChapters({language}: { language: string }): Promise<ReadonlyArray<Chapter>> {
 	const chapters = await getList<Chapter>("chapters", language)
-	return chapters.map((chapter, i) => ({...chapter, number: i + 1}))
+	return chapters.map((chapter, i) => ({
+		...chapter,
+		number: i + 1,
+		pages: chapter.pages.map(page =>
+			({...page, characters: page.characters.map(character => ({...character, image: imageToUrl(character.image)}))})
+		)
+	}))
 }
