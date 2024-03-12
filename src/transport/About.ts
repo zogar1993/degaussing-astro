41d670921d9a8b656381b53f4cf37a346918d523
Facundo Garcia <facundogarcia-ae@team.asana.com>
2024-03-12 18:08:29 -0300

    :lipstick: make styles for characters separate description from rest

--- src/transport/About.ts
+++ src/transport/About.ts
@@ -1,5 +1,10 @@
+// @ts-ignore
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+
 export type About = {
 	title: string
 	banner: string
-	synopsis: Document
+	synopsis: RichTextDocument
 }
+
+export type RichTextDocument = Parameters<typeof documentToHtmlString>[0]
