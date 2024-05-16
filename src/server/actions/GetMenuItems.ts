dfd4e4d99dbc7fe248afac3099d52c3c74c97597
Facundo Garcia <facundogarcia-ae@team.asana.com>
2024-05-15 23:18:42 -0400

    :fire: remove old code for distinguishing icon and text buttons on desktop

--- src/server/actions/GetMenuItems.ts
+++ src/server/actions/GetMenuItems.ts
@@ -11,14 +11,12 @@ export default function getMenuItems({ path }: { path: string }): Array<MenuItem
 			selected: pathname === "/",
 			text: t("menu.home"),
 			icon: "/icons/home.svg",
-			desktop: "text"
 		},
 		{
 			href: localizePath("/chapters"),
 			selected: pathname.startsWith("/chapters"),
 			text: t("menu.chapters"),
 			icon: "/icons/chapters.svg",
-			desktop: "text",
 			isSubPage: pathname.startsWith("/chapters") && pathname !== "/chapters"
 		},
 		{
@@ -26,21 +24,18 @@ export default function getMenuItems({ path }: { path: string }): Array<MenuItem
 			selected: pathname === "/characters",
 			text: t("menu.characters"),
 			icon: "/icons/characters.svg",
-			desktop: "text"
 		},
 		{
 			href: localizePath("/about"),
 			selected: pathname === "/about",
 			text: t("menu.about"),
 			icon: "/icons/about.svg",
-			desktop: "text"
 		},
 		{
 			href: localizePath(pathname, i18next.language === "es" ? "en" : "es"),
 			selected: false,
 			text: t("menu.language"),
 			icon: "/icons/language.svg",
-			desktop: "text"
 		}
 	]
 }
@@ -50,6 +45,5 @@ type MenuItem = {
 	selected: boolean
 	text: string
 	icon: string
-	desktop: "text" | "icon"//TODO icon seems to not be used anymore
 	isSubPage?: boolean
 }
