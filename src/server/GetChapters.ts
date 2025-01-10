3bd1e4cc1a2372ea0be5844ae8842b455e62ff1a
Facundo Garcia <facundogarcia-ae@team.asana.com>
2025-01-10 00:36:44 -0300

    :bug: fix bug that made images be heavier than needed

--- src/server/GetChapters.ts
+++ src/server/GetChapters.ts
@@ -10,13 +10,12 @@ export async function getChapters({ language }: { language: string }): Promise<R
 		number: i + 1,
 		pages: [chapter.cover, ...chapter.pages].map((page, i) => ({
 				...page,
+				image: imageToUrl(page.image),
 				createdAt: new Date(page.createdAt),
-				characters: (page.characters || []).map(fixCharacterImageUrl),
+				characters: (page.characters || []).map(character => ({ ...character, image: imageToUrl(character.image) })),
 				number: i
 			})
-		).filter(page => page.createdAt < Date.now())
+		).filter(page => page.createdAt < new Date())
 	})).filter(chapter => chapter.pages.length > 0)
 }
 
-const fixCharacterImageUrl = (character: { name: string, image: string }) =>
-	({ ...character, image: imageToUrl(character.image) })
