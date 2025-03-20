63ea7a278fe695c3f8c26f78d1b1f058dcf430ed
Facundo Garcia <facundogarcia-ae@team.asana.com>
2025-03-20 01:59:44 -0300

    :sparkles: add automatic bookmarking

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config"
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
