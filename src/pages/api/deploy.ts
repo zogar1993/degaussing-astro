e9ed6dea53f61971482b8f9f44f7a82e98ccc747
Facundo Garcia <facundogarcia-ae@team.asana.com>
2025-03-31 02:48:52 -0300

    :wrench: renamed API_ACCESS_TOKEN to CRON_SECRET

--- src/pages/api/deploy.ts
+++ src/pages/api/deploy.ts
@@ -1,11 +1,11 @@
 export const prerender = false
 
 const VERCEL_ACCESS_TOKEN = import.meta.env.VERCEL_ACCESS_TOKEN!
-const API_ACCESS_TOKEN = import.meta.env.API_ACCESS_TOKEN!
+const CRON_SECRET = import.meta.env.CRON_SECRET!
 const GITHUB_REPO_ID = import.meta.env.GITHUB_REPO_ID!
 
 export async function GET({ request }) {
-	if (request.headers.get("Authorization") !== `Bearer ${API_ACCESS_TOKEN}`)
+	if (request.headers.get("Authorization") !== `Bearer ${CRON_SECRET}`)
 		return new Response(null, { status: 401 })
 
 	const response = await fetch("https://api.vercel.com/v13/deployments", {
