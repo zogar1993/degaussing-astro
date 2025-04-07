cc3416f47ce99ac75ad65db5b20df1a6d9cc6c43
Facundo Garcia <facundogarcia-ae@team.asana.com>
2025-04-07 17:57:39 -0300

    :art: refactor ids so that only shared ones are extracted

--- src/web/ID.ts
+++ src/web/ID.ts
@@ -1,6 +1,4 @@
 export const ID = {
 	MAIN: "main",
 	TRANSCRIPT: "transcript",
-	LANGUAGE_SELECTOR_DESKTOP: "language-selector-desktop",
-	LANGUAGE_SELECTOR_MOBILE: "language-selector-mobile",
 } as const
