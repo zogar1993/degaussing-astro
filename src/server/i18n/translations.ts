f857fc16cb2c031179587a0b13a274df391d8fb9
Facundo Garcia <facundogarcia-ae@team.asana.com>
2025-04-09 23:57:23 -0300

    :wheelchair: alt text for image now redirects to transcript

--- src/server/i18n/translations.ts
+++ src/server/i18n/translations.ts
@@ -47,6 +47,7 @@ export const translations = {
 
 		"seo.description": "Our young friends escape their hometown and make a run through the continent of Gaoan, but it seems like nowhere is really safe! Where could a mage go to avoid persecution? What secrets do Alan's sword hold? Old grudges are brewing beneath the surface and war is coming from overseas.",
 
+		"transcript.image.alt-text": "Comic page. Complete transcript for the content below.",
 		"transcript.title": "Transcript",
 		"transcript.no-description": "This page hasn't been transcribed yet.",
 		"transcript.button.expanded-false": "Open transcript",
@@ -79,8 +80,8 @@ export const translations = {
 		"menu.about": "Acerca de",
 		"menu.language": "Lenguaje",
 
-		"strip.heading-main": "Página del comic",
-		"strip.heading-main-home": "Última página del comic",
+		"strip.heading-main": "Página del cómic",
+		"strip.heading-main-home": "Última página del cómic",
 		
 		"comic.chapter.singular": "Capítulo",
 		"comic.page.singular": "Página",
@@ -94,7 +95,7 @@ export const translations = {
 		"characters.years-old": "años",
 
 		"landmarks.navigation-page": "Navegación de la página",
-		"landmarks.navigation-comic": "Navegación del comic",
+		"landmarks.navigation-comic": "Navegación del cómic",
 		"landmarks.next-update": "Próxima actualización",
 		"landmarks.characters": "Personajes",
 		"landmarks.donations": "Donaciones",
@@ -114,6 +115,7 @@ export const translations = {
 
 		"seo.description": "Nuestros jovenes amigos escapan de su pueblo y emprenden un viaje a través del continente de Gaoan, ¡Pero ningún lugar es seguro! ¿Donde podría ir un mago para evitar ser perseguido? ¿Que secretos guarda la espada de Alan? Antiguos rencores no se olvidan, cruzando el mar se preparan para la guerra.",
 
+		"transcript.image.alt-text": "Pagina del cómic. La transcripción completa del contenido se encuentra debajo.",
 		"transcript.title": "Transcripción",
 		"transcript.no-description": "Aún no se ha realizado la descripción de esta página.",
 		"transcript.button.expanded-false": "Abrir transcripción",
