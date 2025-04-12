31136f2538bc7c5114605c2bc94a69e9462ea223
Facundo Garcia <facundogarcia-ae@team.asana.com>
2025-04-12 18:30:48 -0300

    :mag: add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config"
 import vercel from "@astrojs/vercel";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://degaussingcomic.com",
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
@@ -10,12 +13,13 @@ export default defineConfig({
 
   image: {
       domains: ["images.ctfassets.net"]
-	},
+    },
 
   devToolbar: {
       enabled: false
-	},
+    },
 
   output: "static",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [sitemap()]
 })
