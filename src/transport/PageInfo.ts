65426e474dcc39142fdb2bd66eb86a2b2e1647b9
Facundo Garcia <facundogarcia-ae@team.asana.com>
2025-04-14 22:31:58 -0300

    :fire: remove old code and comments

--- src/transport/PageInfo.ts
+++ src/transport/PageInfo.ts
@@ -2,7 +2,6 @@ import type { RelatedPages } from "@server/Utils"
 import type { RichTextDocument } from "@server/contentful/types/RichTextDocument"
 import type { Strip } from "@transport/Chapter"
 
-//TODO refactor types
 export type PageInfo = RelatedPages & {
 	image: string,
 	characters: Strip["characters"],
