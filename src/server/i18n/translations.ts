45dacbc44e2aea59c281974d7fdbcf34fb020b72
Facundo Garcia <facundogarcia-ae@team.asana.com>
2026-06-27 01:37:18 -0300

    :technologist: refactor translations and their types

--- src/server/i18n/translations.ts
+++ src/server/i18n/translations.ts
@@ -1,163 +1,162 @@
-export const translations = {
-	en: {
-		"interaction.skip-to-main": "Skip to main content",
+const EN_TEXT = {
+	"interaction.skip-to-main": "Skip to main content",
 
-		"interaction.main-menu": "Main menu",
-		"interaction.close": "Close",
-		"interaction.open": "Open Menu",
+	"interaction.main-menu": "Main menu",
+	"interaction.close": "Close",
+	"interaction.open": "Open Menu",
 
-		"menu.chapters": "Chapters",
-		"menu.about": "About",
-		"menu.about.characters": "Characters",
-		"menu.about.story": "Story",
-		"menu.about.authors": "Authors",
-		"menu.about.faq": "FAQ",
-		"menu.language": "Language",
+	"menu.chapters": "Chapters",
+	"menu.about": "About",
+	"menu.about.characters": "Characters",
+	"menu.about.story": "Story",
+	"menu.about.authors": "Authors",
+	"menu.about.faq": "FAQ",
+	"menu.language": "Language",
 
-		"comic.chapter.singular": "Chapter",
-		"comic.page.singular": "Page",
-		"comic.cover.singular": "Cover",
+	"comic.chapter.singular": "Chapter",
+	"comic.page.singular": "Page",
+	"comic.cover.singular": "Cover",
 
-		"announcement.new-pages.text": "There are {number} new pages since your last visit!",
+	"announcement.new-pages.text": "There are {number} new pages since your last visit!",
 
-		"strip.heading-main": "Comic page",
+	"strip.heading-main": "Comic page",
 
-		"chapters.pages": "pages",
+	"chapters.pages": "pages",
 
-		"characters.age": "Age",
-		"characters.likes": "Likes",
-		"characters.dislikes": "Dislikes",
-		"characters.years-old": "years old",
+	"characters.age": "Age",
+	"characters.likes": "Likes",
+	"characters.dislikes": "Dislikes",
+	"characters.years-old": "years old",
 
-		"landmarks.navigation-page": "Page navigation",
-		"landmarks.navigation-comic": "Comic navigation",
-		"landmarks.next-update": "Next update",
-		"landmarks.characters": "Characters",
-		"landmarks.donations": "Donations",
-		"landmarks.navigation-social": "Social navigation",
-		"landmarks.comments": "Comments",
-		"landmarks.social": "Social",
-		"landmarks.newsletter": "Newsletter",
-		"landmarks.strip-aside": "Current page information",
-		"landmarks.author": "Author",
+	"landmarks.navigation-page": "Page navigation",
+	"landmarks.navigation-comic": "Comic navigation",
+	"landmarks.next-update": "Next update",
+	"landmarks.characters": "Characters",
+	"landmarks.donations": "Donations",
+	"landmarks.navigation-social": "Social navigation",
+	"landmarks.comments": "Comments",
+	"landmarks.social": "Social",
+	"landmarks.newsletter": "Newsletter",
+	"landmarks.strip-aside": "Current page information",
+	"landmarks.author": "Author",
 
-		"newsletter.title": "Newsletter",
-		"newsletter.description": "Subscribe for new release and other updates!",
-		"newsletter.subscribe": "Subscribe",
-		"newsletter.success": "You have successfully joined our newsletter!",
-		"newsletter.error": `Something went wrong! Please try again later, or <a href="https://github.com/zogar1993/degaussing-astro/issues">post an issue</a> if the problem persists.`,
-		"newsletter.recaptcha-policy": `*This form is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy">Privacy Policy</a> and <a href="https://policies.google.com/terms">Terms of Service</a> apply.`,
+	"newsletter.title": "Newsletter",
+	"newsletter.description": "Subscribe for new release and other updates!",
+	"newsletter.subscribe": "Subscribe",
+	"newsletter.success": "You have successfully joined our newsletter!",
+	"newsletter.error": `Something went wrong! Please try again later, or <a href="https://github.com/zogar1993/degaussing-astro/issues">post an issue</a> if the problem persists.`,
+	"newsletter.recaptcha-policy": `*This form is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy">Privacy Policy</a> and <a href="https://policies.google.com/terms">Terms of Service</a> apply.`,
 
-		"seo.description": "Our young friends escape their hometown and make a run through the continent of Gaoan, but it seems like nowhere is really safe! Where could a mage go to avoid persecution? What secrets do Alan's sword hold? Old grudges are brewing beneath the surface and war is coming from overseas.",
+	"seo.description": "Our young friends escape their hometown and make a run through the continent of Gaoan, but it seems like nowhere is really safe! Where could a mage go to avoid persecution? What secrets do Alan's sword hold? Old grudges are brewing beneath the surface and war is coming from overseas.",
 
-		"transcript.image.alt-text": "Comic page. Complete transcript for the content below.",
-		"transcript.title": "Transcript",
-		"transcript.button.expanded-false": "Open transcript",
-		"transcript.button.expanded-true": "Close transcript",
+	"transcript.image.alt-text": "Comic page. Complete transcript for the content below.",
+	"transcript.title": "Transcript",
+	"transcript.button.expanded-false": "Open transcript",
+	"transcript.button.expanded-true": "Close transcript",
 
-		"secondary-nav.first": "First page",
-		"secondary-nav.previous": "Previous page",
-		"secondary-nav.next": "Next page",
-		"secondary-nav.last": "Last page",
-		"secondary-nav.back": "Back",
+	"secondary-nav.first": "First page",
+	"secondary-nav.previous": "Previous page",
+	"secondary-nav.next": "Next page",
+	"secondary-nav.last": "Last page",
+	"secondary-nav.back": "Back",
 
-		"heading.about.story": "About the story",
-		"heading.about.authors": "About the authors",
-		"heading.about.characters": "About the characters",
-		"heading.about.faq": "Frequently asked questions",
+	"heading.about.story": "About the story",
+	"heading.about.authors": "About the authors",
+	"heading.about.characters": "About the characters",
+	"heading.about.faq": "Frequently asked questions",
 
-		"url.chapters": "chapters",
-		"url.pages": "pages",
-		"url.characters": "characters",
-		"url.about": "about",
-		"url.story": "story",
-		"url.authors": "authors",
-		"url.faq": "faq",
-	},
-	es: {
-		"interaction.skip-to-main": "Saltar al contenido principal",
-
-		"interaction.main-menu": "Menú principal",
-		"interaction.close": "Cerrar",
-		"interaction.open": "Abir Menu",
-
-		"menu.chapters": "Capítulos",
-		"menu.about": "Acerca de",
-		"menu.about.characters": "Personajes",
-		"menu.about.story": "Historia",
-		"menu.about.authors": "Autores",
-		"menu.about.faq": "FAQ",
-		"menu.language": "Lenguaje",
-
-		"announcement.new-pages.text": "¡Hay {number} nuevas páginas desde tu última visita!",
-
-		"strip.heading-main": "Página del cómic",
-
-		"comic.chapter.singular": "Capítulo",
-		"comic.page.singular": "Página",
-		"comic.cover.singular": "Portada",
-
-		"chapters.pages": "páginas",
-
-		"characters.age": "Edad",
-		"characters.likes": "Le gusta",
-		"characters.dislikes": "Le disgusta",
-		"characters.years-old": "años",
-
-		"landmarks.navigation-page": "Navegación de la página",
-		"landmarks.navigation-comic": "Navegación del cómic",
-		"landmarks.next-update": "Próxima actualización",
-		"landmarks.characters": "Personajes",
-		"landmarks.donations": "Donaciones",
-		"landmarks.navigation-social": "Navegación social",
-		"landmarks.comments": "Comentarios",
-		"landmarks.social": "Social",
-		"landmarks.newsletter": "Novedades",
-		"landmarks.strip-aside": "Information de página actual",
-		"landmarks.author": "Autor",
-
-		"newsletter.title": "Novedades",
-		"newsletter.description": "¡Enterate cuando salen nuevas paginas y otras noticias!",
-		"newsletter.subscribe": "Suscribir",
-		"newsletter.success": "¡Te suscribiste al newsletter exitosamente!",
-		"newsletter.error": "Ha ocurrido un error. Inténtelo nuevamente más tarde, o <a href=\"https://github.com/zogar1993/degaussing-astro/issues\">reporte el error</a> si este persiste.",
-		"newsletter.recaptcha-policy": `*Este formulario está protegido por reCAPTCHA de Google y aplican su <a href="https://policies.google.com/privacy">Política de Privacidad</a> y sus <a href="https://policies.google.com/terms">Términos de Servicio</a>.`,
-
-		"seo.description": "Nuestros jovenes amigos escapan de su pueblo y emprenden un viaje a través del continente de Gaoan, ¡Pero ningún lugar es seguro! ¿Donde podría ir un mago para evitar ser perseguido? ¿Que secretos guarda la espada de Alan? Antiguos rencores no se olvidan, cruzando el mar se preparan para la guerra.",
-
-		"transcript.image.alt-text": "Pagina del cómic. La transcripción completa del contenido se encuentra debajo.",
-		"transcript.title": "Transcripción",
-		"transcript.button.expanded-false": "Abrir transcripción",
-		"transcript.button.expanded-true": "Cerrar transcripción",
-
-		"secondary-nav.first": "Primera página",
-		"secondary-nav.previous": "Página anterior",
-		"secondary-nav.next": "Página siguiente",
-		"secondary-nav.last": "Última página",
-		"secondary-nav.back": "Atrás",
-
-		"heading.about.story": "Acerca de la historia",
-		"heading.about.authors": "Acerca de los autores",
-		"heading.about.characters": "Acerca de los personajes",
-		"heading.about.faq": "Preguntas frecuentes",
-
-		"url.chapters": "capitulos",
-		"url.pages": "paginas",
-		"url.characters": "personajes",
-		"url.about": "acerca-de",
-		"url.story": "historia",
-		"url.authors": "autores",
-		"url.faq": "faq",
-	}
+	"url.chapters": "chapters",
+	"url.pages": "pages",
+	"url.characters": "characters",
+	"url.about": "about",
+	"url.story": "story",
+	"url.authors": "authors",
+	"url.faq": "faq"
 } as const
 
-//TODO this is a bit ugly, lets see if we can make it better. typpings of languages overall is bad
-export const URL_PARTS = [
-	{ en: translations["en"]["url.chapters"], es: translations["es"]["url.chapters"] },
-	{ en: translations["en"]["url.pages"], es: translations["es"]["url.pages"] },
-	{ en: translations["en"]["url.characters"], es: translations["es"]["url.characters"] },
-	{ en: translations["en"]["url.about"], es: translations["es"]["url.about"] },
-	{ en: translations["en"]["url.story"], es: translations["es"]["url.story"] },
-	{ en: translations["en"]["url.authors"], es: translations["es"]["url.authors"] },
-	{ en: translations["en"]["url.faq"], es: translations["es"]["url.faq"] },
-] as const
+const ES_TEXT = {
+	"interaction.skip-to-main": "Saltar al contenido principal",
+
+	"interaction.main-menu": "Menú principal",
+	"interaction.close": "Cerrar",
+	"interaction.open": "Abir Menu",
+
+	"menu.chapters": "Capítulos",
+	"menu.about": "Acerca de",
+	"menu.about.characters": "Personajes",
+	"menu.about.story": "Historia",
+	"menu.about.authors": "Autores",
+	"menu.about.faq": "FAQ",
+	"menu.language": "Lenguaje",
+
+	"announcement.new-pages.text": "¡Hay {number} nuevas páginas desde tu última visita!",
+
+	"strip.heading-main": "Página del cómic",
+
+	"comic.chapter.singular": "Capítulo",
+	"comic.page.singular": "Página",
+	"comic.cover.singular": "Portada",
+
+	"chapters.pages": "páginas",
+
+	"characters.age": "Edad",
+	"characters.likes": "Le gusta",
+	"characters.dislikes": "Le disgusta",
+	"characters.years-old": "años",
+
+	"landmarks.navigation-page": "Navegación de la página",
+	"landmarks.navigation-comic": "Navegación del cómic",
+	"landmarks.next-update": "Próxima actualización",
+	"landmarks.characters": "Personajes",
+	"landmarks.donations": "Donaciones",
+	"landmarks.navigation-social": "Navegación social",
+	"landmarks.comments": "Comentarios",
+	"landmarks.social": "Social",
+	"landmarks.newsletter": "Novedades",
+	"landmarks.strip-aside": "Information de página actual",
+	"landmarks.author": "Autor",
+
+	"newsletter.title": "Novedades",
+	"newsletter.description": "¡Enterate cuando salen nuevas paginas y otras noticias!",
+	"newsletter.subscribe": "Suscribir",
+	"newsletter.success": "¡Te suscribiste al newsletter exitosamente!",
+	"newsletter.error": "Ha ocurrido un error. Inténtelo nuevamente más tarde, o <a href=\"https://github.com/zogar1993/degaussing-astro/issues\">reporte el error</a> si este persiste.",
+	"newsletter.recaptcha-policy": `*Este formulario está protegido por reCAPTCHA de Google y aplican su <a href="https://policies.google.com/privacy">Política de Privacidad</a> y sus <a href="https://policies.google.com/terms">Términos de Servicio</a>.`,
+
+	"seo.description": "Nuestros jovenes amigos escapan de su pueblo y emprenden un viaje a través del continente de Gaoan, ¡Pero ningún lugar es seguro! ¿Donde podría ir un mago para evitar ser perseguido? ¿Que secretos guarda la espada de Alan? Antiguos rencores no se olvidan, cruzando el mar se preparan para la guerra.",
+
+	"transcript.image.alt-text": "Pagina del cómic. La transcripción completa del contenido se encuentra debajo.",
+	"transcript.title": "Transcripción",
+	"transcript.button.expanded-false": "Abrir transcripción",
+	"transcript.button.expanded-true": "Cerrar transcripción",
+
+	"secondary-nav.first": "Primera página",
+	"secondary-nav.previous": "Página anterior",
+	"secondary-nav.next": "Página siguiente",
+	"secondary-nav.last": "Última página",
+	"secondary-nav.back": "Atrás",
+
+	"heading.about.story": "Acerca de la historia",
+	"heading.about.authors": "Acerca de los autores",
+	"heading.about.characters": "Acerca de los personajes",
+	"heading.about.faq": "Preguntas frecuentes",
+
+	"url.chapters": "capitulos",
+	"url.pages": "paginas",
+	"url.characters": "personajes",
+	"url.about": "acerca-de",
+	"url.story": "historia",
+	"url.authors": "autores",
+	"url.faq": "faq"
+} as const
+
+type Translation = Record<keyof typeof EN_TEXT, string>
+
+export const translations: Record<"en" | "es", Translation> = {
+	en: EN_TEXT,
+	es: ES_TEXT
+} as const
+
+export const URL_PARTS = (Object.keys(EN_TEXT) as Array<keyof typeof EN_TEXT>)
+	.filter(key => key.startsWith("url."))
+	.map(key => ({ en: EN_TEXT[key], es: ES_TEXT[key] }))
